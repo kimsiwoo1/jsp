e30300ad335a7d481f399adc29667f8d0bfbc971
kimsiwoo1 <kimsiwoo1.snowis13@gmail.com>
2019-08-21 17:25:09 +0900

    사용자 페이징 리스트 개발 		- 페이징 처리 쿼리 (order by, rownum) 		- service 메서드에서 복수개의 dao 메소드 		- 페이지네이션 처리 			-totalCnt

--- src/main/java/kr/or/ddit/user/service/UserService.java
+++ src/main/java/kr/or/ddit/user/service/UserService.java
@@ -1,9 +1,12 @@
 package kr.or.ddit.user.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
+import kr.or.ddit.common.model.Page;
 import kr.or.ddit.user.model.User;
 import kr.or.ddit.user.repository.IUserDao;
 import kr.or.ddit.user.repository.UserDao;
@@ -38,6 +41,21 @@ public class UserService implements IUserService {
 		SqlSession sqlSession = MybatisUtil.getSession();
 		List<User> userList = userDao.getCntUserList(sqlSession);
 		return userList;
+	}
+
+	@Override
+	public Map<String, Object> getUserPagingList(Page page) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		SqlSession sqlSession = MybatisUtil.getSession();
+		
+		List<User> userList = userDao.getUserPagingList(sqlSession, page);
+		int totalCnt = userDao.getUserTotalCnt(sqlSession);
+		
+		map.put("userList", userList);
+		map.put("paginationSize", (int)Math.ceil((double)totalCnt / page.getPagesize()));
+		sqlSession.close();
+		return map;
 	}
 
 }
